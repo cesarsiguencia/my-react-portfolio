7ea43842596633612625679a1704c9270cc18150
cesarsiguencia <cesar.siguencia@gmail.com>
2022-05-22 23:25:18 -0400

    uploading resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import hello from '../'
+
 const Resume = () => {
 
     return(
         <div>
             <p>Resume!!!</p>
+            <a href="../../cesar-siguencia-resume.pdf" download>Download the PDF</a>
         </div>
     )
 }
