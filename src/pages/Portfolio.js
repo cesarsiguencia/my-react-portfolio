e929999fa074a750243ae05745341d4c85ceb998
cesarsiguencia <cesar.siguencia@gmail.com>
2022-07-27 21:21:49 -0400

    restructuring app-box dimensions for better visuals

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -7,24 +7,30 @@ const Portfolio = () => {
     return(
         <div className="body-divs">
             <h3>Portfolio</h3>
-            <p className="body-texts">Click on the image below to deploy application link</p>
+            <p className="body-texts">Hover over image to see a short description from one of my featured apps. 
+            Click on the image below to deploy application link.</p>
 
             <div>
-                <div>
+                <div className="app-box">
                     <div className="container_wrapper">
-                        <a target="_blank" href='https://nft-y.herokuapp.com/'>
-                        <img className="container--picture" src={require(`../../src/media/app-images/nft-y.png`)} width="100%">
-                        </img>  
-                        <div className="container--overlay">
-                            <p className="container---blurb"> This is a test for NFT-Y, lets see if this actually works, and if does, I'm going to be so phycsed! Please do!</p>  
-               
-                        </div> 
-                        </a>
+                        <div className="app-description">
+                            <h5>NFT-y</h5>
+                            <p>Back end developer</p>
+                        </div>
+                    </div>
+                    <div className="container_wrapper">
+                        <div className="app_image">
+                 
+                            <img className="container--picture" src={require(`../../src/media/app-images/nft-y.png`)}>
+                            </img>  
+                            <div className="container--overlay">
+                                <p className="container---blurb"> This is a test for NFT-Y, lets see if this actually works, and if does, I'm going to be so phycsed! Please do!</p>  
+                            </div> 
+                                <a target="_blank" href='https://nft-y.herokuapp.com/'></a>
+                        </div>
                     </div>
                   
-                    <div>
-                        <h5 className='app-name'>NFT-y</h5>
-                    </div>
+
 
                 </div>
 
@@ -33,15 +39,20 @@ const Portfolio = () => {
             </div>
 
             <div>
-                <div>
-                    <a  target="_blank" href='https://bootcamp-social.herokuapp.com/' >
-                        <img  className="container" src={require(`../../src/media/app-images/Bootcamp-Social.png`)} width="45%"></img>    
-                        
-                    </a>
+                <div class="app-box">
                     <div>
-                        <h5 className='app-name'>Bootcamp-Social</h5>
+                         <h5 className='app-name'>Bootcamp-Social</h5>
                     </div>
-
+                    <div className="container_wrapper">
+                        <a target="_blank" href='https://bootcamp-social.herokuapp.com/' >
+                            <img  className="container" src={require(`../../src/media/app-images/Bootcamp-Social.png`)} width="100%"></img>   
+                            <div className="container--overlay">
+                                <p className="container---blurb"> This is a test for NFT-Y, lets see if this actually works, and if does, I'm going to be so phycsed! Please do!</p>  
+                            </div> 
+                        </a>
+                    </div>
+                  
+        
                 </div>
 
 
