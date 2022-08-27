ae6f44ca5f5a7a371683d6c9052e80766b99aedb
cesarsiguencia <cesar.siguencia@gmail.com>
2022-08-27 08:19:30 -0400

    removed redundant social links

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,11 +11,13 @@ const Contact = () => {
 return (
     <div className="body-divs">
         <h3>Contact</h3>
-        <p className="body-texts">Please find my contact info here. I will get back within twenty four hours.</p>
-        
-        <p className="body-texts"> The best way to reach me is by email or through a LinkedIn message. All available personal links can be found at the footer of this website.</p>
+        <p className="body-texts">Let's connect! Please find my contact info here. I will get back within twenty four hours. Click on email to begin message.</p>
 
-        <div className="logos-contactpg">
+        <a className="links" href="mailto:cesar.siguencia@gmail.com">cesar.siguencia@gmail.com</a>
+        
+        <p className="body-texts"> The best way to reach me is by email or through a LinkedIn message. I can also be reached through Github and Instagram, links provided at the footer.</p>
+
+        {/* <div className="logos-contactpg">
                 <a className="icon-align" href="https://github.com/cesarsiguencia" target="_blank" rel="noopener noreferrer">
                     <img className="logos-icons" src={github} width="50"/>
                 </a> 
@@ -27,11 +29,9 @@ return (
                 <a className="icon-align" href="https://www.linkedin.com/in/c%C3%A9sar-siguencia-b71aabba/" target="_blank" rel="noopener noreferrer">
                     <img className="logos-icons" src={linkedin} width="50"/>
                 </a>
-        </div>
+        </div> */}
 
-        <p className="body-texts">Click on email to begin message.</p>
-
-        <a className="links" href="mailto:cesar.siguencia@gmail.com">cesar.siguencia@gmail.com</a>
+   
 
         <br>
         </br>
@@ -39,7 +39,7 @@ return (
         </br>
         
         <div>
-            <img src={riverbank_morning} width="75%"></img>
+            <img className="profile" src={riverbank_morning} width="75%"></img>
         </div>
        
     </div>
