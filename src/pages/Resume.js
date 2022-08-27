d0ffdd3eaccf92f2276501a43ab7aa23282a8136
cesarsiguencia <cesar.siguencia@gmail.com>
2022-08-26 23:54:41 -0400

    technology logos

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -22,60 +22,52 @@ const Resume = () => {
 
     return(
         <div className="body-divs">
-            <h3>Resume</h3>
-            <p className="body-texts">Click on the image below to download the PDF version</p>
-            <a href={resume} download><img src={image} width="50%"/>
-            </a>
-
             <div className="skills">
-                <h3>Web Development Skills</h3>
-                <div className="skills--lists">
-                    <div className="skills---divs">
-                        <h5>Front End</h5>
-                        <div className="technologies">
-                            <img className="technologies-logos" src={html}/>
-                            <img className="technologies-logos" src={css}/>
-                            <img className="technologies-logos" src={js}/>
-                            <img className="technologies-logos" src={jquery}/>
-                            <img className="technologies-logos" src={bootstrap}/>
-                            <img className="technologies-logos" src={react}/>
+                <div className="skills-blocks"> 
+                    <h3>Resume</h3>
+                        <p className="body-texts">Click on the image below to download the PDF version</p>
+                        <a href={resume} download><img src={image} width="75%"/>
+                        </a>
+                </div>
+                
+                <div className="skills-blocks">
+                    <h3>Web Development Skills</h3>
 
+                    <div className="skills--lists">
+                        <div>
+                            <h5>Front End</h5>
+                 
+                            <div className="technologies">
+                                <img className="technologies-logos" src={html}/>
+                                <img className="technologies-logos" src={css}/>
+                                <img className="technologies-logos" src={js}/>
+                                <img className="technologies-logos" src={jquery}/>
+                                <img className="technologies-logos" src={bootstrap}/>
+                                <img className="technologies-logos" src={react}/>
+
+                               
+                            </div>
                         </div>
-                            {/* <ul>
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                                <li>Jquery</li>
-                                <li>Bootstrap</li>
-                                <li>React</li>
-                            </ul> */}
+                        <div>
+                            <h5>Back End</h5>
+                            <div className="technologies">
+                                <img className="technologies-logos" src={api}/>
+                                <img className="technologies-logos" src={node}/>
+                                <img className="technologies-logos" src={express}/>
+                                <img className="technologies-logos" src={mysql}/>
+                                <img className="technologies-logos" src={sequelize}/>
+                                <img className="technologies-logos" src={mongodb}/>
+                                <img className="technologies-logos" src={mongoose}/>
+                                <img className="technologies-logos" src={graphql}/>
+
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="skills---divs">
-                        <h5>Back End</h5>
-                        <div className="technologies">
-                            <img className="technologies-logos" src={api}/>
-                            <img className="technologies-logos" src={node}/>
-                            <img className="technologies-logos" src={express}/>
-                            <img className="technologies-logos" src={mysql}/>
-                            <img className="technologies-logos" src={sequelize}/>
-                            <img className="technologies-logos" src={mongodb}/>
-                            <img className="technologies-logos" src={mongoose}/>
-                            <img className="technologies-logos" src={graphql}/>
-                        </div>
-                            {/* <ul>
-                                <li>APIs</li>
-                                <li>Node</li>
-                                <li>Express</li>
-                                <li>MySQL, Sequelize</li>
-                                <li>MongoDB</li>
-                                <li>Mongoose</li>
-                                <li>REST</li>
-                                <li>GraphQL</li>
-                            </ul> */}
-                    </div>
                 </div>
             </div>
+      
+           
         </div>
 
         
