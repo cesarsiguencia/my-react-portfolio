fd69f048deb4fe29dd3c55fba50f066fdd32301d
cesarsiguencia <cesar.siguencia@gmail.com>
2022-09-06 07:05:51 -0400

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,13 @@ import home from "../media/me/home.png"
 
 import home2 from "../media/me/home-orange.png"
 
-// import portrait1 from "../media/me/portrait-1.png"
+import portrait1 from "../media/me/portrait-1.png"
 
-// import portrait2 from "../media/me/portrait-2.png"
+import portrait2 from "../media/me/portrait-2.png"
 
 // import { backgroundEffect } from "../utils/helpers"
 
-import Portraits from "../Components/Backgrounds/Portraits.js"
+// import Portraits from "../Components/Backgrounds/Portraits.js"
 
 // import Bridges from "../Components/Backgrounds/Landscapes.js"
 
@@ -72,6 +72,27 @@ const Home = () => {
         
     //   }) 
 
+    let backgroundWidth
+
+    window.addEventListener("mousemove", function(e){
+        const backgroundDiv = document.querySelector(".hello")
+        // const xAxis = e.clientX
+  
+        if(backgroundDiv){
+            
+            const xAxis = e.clientX
+  
+  
+            const name = xAxis
+        
+            console.log(name)
+            backgroundWidth = name + "px"
+            backgroundDiv.style.width = backgroundWidth
+            console.log(backgroundWidth)
+        }
+        
+      }) 
+
 
 
       return (
@@ -124,28 +145,54 @@ const Home = () => {
         // </div>
 
         <div className="home-div">
-            <Portraits></Portraits>
+            <div className="home-bcgs">
+                <img className="bcg-2" id="bcg-2" src={portrait2} width="100%"/>
+
+                <div className="hidden-intro">
+    
+                    <h3>Welcome to my page</h3>
+
+                    <h5>Hello, Hola, 你好!</h5>
+   
+
+                    <div className="hidden-blurb">
+                        <h6 className="body-texts hidden-texts">Thank you for visiting my page. I hope you enjoy learning about me and will connect with me soon. Click the links above to be redirected. </h6>
+    
         
+    
+                        <h6 className="body-texts">And welcome to my favorite view ever located footsteps from my home, Hamilton Heights, NYC! Scroll down below. </h6>
+
+                    </div>
+                </div>
+
+                <div className="hello">
+                    <img className="bcg-1" id="bcg-1" src={portrait1} />
+                    <div className="hidden-intro">
+                        <div>
+                            <h6 className="body-texts hidden-texts">* Slide Mouse Horizontally! *</h6>
+                        </div>
+        
+
+                    </div>
+                </div>
+
+            </div>
 
             <div className="home-bcgs">
                 <img className="bcg-2" id="bcg-2" src={home} width="100%"/>
 
                 <div className="personal">
                     <img className="bcg-1" id="bcg-1" src={home2} width="100%"/>
-                    <div>
-                        <p className="body-texts">* Slide Mouse Horizontally! *</p>
-              
-                    </div>
+
                 </div>
 
             </div>
-
             
         </div>
-    )
-    
 
+    )
 
 }
 
 export default Home
+
