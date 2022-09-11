0f8ded0a8e44ecfa73f5a4d0452fd8d4eab1ddb2
cesarsiguencia <cesar.siguencia@gmail.com>
2022-09-10 22:42:34 -0400

    new change to bcg-1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -58,8 +58,8 @@ const Home = () => {
                     </div>
                 </div>
 
-                <div className="hello">
-                    <img className="bcg-1" id="bcg-1" width="100" src={portrait1}/>
+                <div className="hello" width="100">
+                    <img className="bcg-1" id="bcg-1"  src={portrait1}/>
                     <div className="hidden-intro">
                         <div>
                             <h6 className="body-texts home-pg-font">* Slide Mouse Horizontally! *</h6>
