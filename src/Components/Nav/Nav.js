20e264d9644e8257fab2d66dcb3757aa8ffdb774
cesarsiguencia <cesar.siguencia@gmail.com>
2023-07-04 22:25:26 -0400

    successful use of useState to change color of current nav page selected

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from 'react-router-dom';
 
@@ -12,18 +12,59 @@ const Nav = () => {
 
   //   }
   // }
- 
-    return(
-        <nav className="Navigator">
-          <div className="mobile-toggle" id="mobile-menu" onClick={hamburgerMenu}>
-            <div className="mobile-options"></div>
-            <div className="mobile-options"></div>
-            <div className="mobile-options"></div>
-          </div>
-          <div className="nav-links">  
-            <h2 id="navtab">
+
+  const pages = [
+    {
+      name: "About Me",
+      url: "aboutme"
+    },
+    {
+      name: "Portfolio",
+      url: "portfolio"
+    },
+    {
+      name: "Contact",
+      url: "contact"
+    },
+    {
+      name: "Resume & Skills",
+      url: "resume"
+    }
+  ]
+
+  // const x = 0
+  const [selectedPage, setSelectedPage] = useState(pages)
+
+
+  return (
+    <nav className="Navigator">
+      <div className="mobile-toggle" id="mobile-menu" onClick={hamburgerMenu}>
+        <div className="mobile-options"></div>
+        <div className="mobile-options"></div>
+        <div className="mobile-options"></div>
+      </div>
+      <div className="nav-links">
+
+        {pages.map((page) => (
+          <h2 id="navtab" key={page.name}
+
+            onClick={() => setSelectedPage(page)}>
+
+            <Link onClick={hamburgerMenu} to={`/my-react-portfolio/${page.url}`}>
+              <h2 className=
+                {`links  
+              
+                ${selectedPage.name === page.name && 'links-selected'}
+              
+              `
+
+                }>{page.name}</h2>
+            </Link>
+          </h2>
+        ))}
+        {/* <h2 id="navtab">
               <Link onClick={hamburgerMenu} to="/my-react-portfolio/aboutme">
-                <h2 className="links links-selected">About Me</h2>
+                <h2 className="links">About Me</h2>
               </Link>
             </h2>
             <h2 id="navtab">
@@ -40,11 +81,14 @@ const Nav = () => {
               <Link onClick={hamburgerMenu} to="/my-react-portfolio/resume">
                 <h2 className="links">Resume & Skills</h2>
               </Link>
-            </h2>
-          </div>
-        
-        </nav>
-    )
+            </h2> */}
+
+
+
+      </div>
+
+    </nav>
+  )
 }
 
 export default Nav
