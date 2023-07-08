ee7c3932542510a3a92025dbe08cabadeadb51e3
cesarsiguencia <cesar.siguencia@gmail.com>
2023-07-08 12:03:29 -0400

    cleaning App and Portfolio components"

--- src/Components/Apps/Apps.js
+++ src/Components/Apps/Apps.js
@@ -1,114 +1,162 @@
 import React from 'react';
-
 import github from '../../media/icons/github-white.png'
 
-    const displayApp = () => {
-        const applications = [
-            // {   
-            //     name: 'Weather-Dashboard', 
-            //     description: 'Need to check full weather conditions of any city quickly? Use this application that will give you just that along with the temperature, wind speed, UV levels, and humidity. The user can also view the conditions up to five days from today. A convienient city search history is displayed for the user to compare conditions across multiple conditions. Application uses local storage and APIs from openweathermap.org',
-            //     technologies: 'Javascript, HTML, CSS, Moment.js, Bootstrap, APIs',
-            // },
-            // {
-            //     name:'Show-Me-My-Schedule',
-            //     description:'Stay organized with all your tasks spread out throughout the day in traditional 9-5 blocks outlining your day. Blocks are color coded to show past, present, and future. Immediately edit a task or clear a task by saving it. ',
-            //     technologies: 'Bootstrap, JavaScript, JQuery, Moment.js, CSS'
-            // },
-            // {
-            //     name: 'Introductory-Javascript-Quiz',
-            //     description: 'Test your javascript skills with this quick quiz I built! Test yourself as many times as you would like as the app saves your scores at the end of each quiz. But hurry, you only have 60 seconds to answer as many javascript questions as possible. Happy studying!',
-            //     technologies: 'Javascript, HTML, CSS'
-            // },
-            // {
-            //     name: 'Save-My-Notes',
-            //     description: 'A classic note taking app that uses back end technologies to save your notes in their appropriate order.',
-            //     technologies: 'NoSQL, Sequelize, Express, RESTful route'
-            // },
-            {
-                name: '10000-Days',
-                description: "A full stack website all about my special event I'm hosting for when I turn 10,000 days old. Learn more about the venue of choice and RSVP to save your spot! In addition, interact with invited guests who have confirmed their registation by sharing posts and loving other guests posts for users to hype up the event until the date arrives! Have fun responsibily!.",
-                technologies: 'Express, Sequelize, MySQL, Bcrypt, Handlebars, Dotenv, HTML, CSS, Javascript',
-                url: 'https://10000-days-production.up.railway.app/home',
-                developer:'Sole Developer'
-            },
-            {
-                name: 'Technify-In-Python',
-                description: "A simple and intuitive social media website for tech enthusiasts to share valuable links with each other. Users can sign up to create, like, and comment on posts. Users have the freedom to delete and any of their posts while being logged in.",
-                technologies: 'Python, Flask, PyMySQL, SQLAlchemy, Bcrypt, Dotenv, Gunicorn, HTML, CSS, Javascript',
-                url: 'https://technify-by-cs.up.railway.app/',
-                developer:'Sole Developer'
-            },
-            { 
-                name: 'Taskmaster-Pro',
-                description: 'Take notes the easy way using Taskmaster Pro, which uses Bootstrap and JQuery technologies. Created tasks can be edited, deleted, and assigned due dates which is color coded to show if a due date is approaching or past due. Tasks can also be dragged around the application interface to various statuses, including "Completed", "In Progress" or deleted to the trash bin below.',
-                technologies: 'Javascript, JQuery, Moment.js,, Bootstrap, HTML, CSS',
-                url: 'https://cesarsiguencia.github.io/taskmaster-pro/',
-                developer:'Sole Developer'
-            },
-            {
-                name: "Dine-N'-Wine",
-                description: 'An application representing multiple hot plate recipes from all over the world! Users are able to save receipes that they like under "Favorites", filter receipes by category, or allow the application to pick one by random. In addition, users can look up for macronutrient information, ingredients, and amount per ingredient needed for the recipe that they choose.',
-                technologies: 'HTML, CSS, Javascript, APIs',
-                url: 'https://cesarsiguencia.github.io/dine-n-wine/',
-                developer:' Front End Developer'
-            }
-        ]
+const displayApp = () => {
+    const applications = [
+        {
+            name: '10000-Days',
+            description: "A full stack website all about my special event I'm hosting for when I turn 10,000 days old. Learn more about the venue of choice and RSVP to save your spot! In addition, interact with invited guests who have confirmed their registation by sharing posts and loving other guests posts for users to hype up the event until the date arrives! Have fun responsibily!",
+            technologies: 'Express, Sequelize, MySQL, Bcrypt, Handlebars, Dotenv, HTML, CSS, Javascript',
+            url: 'https://10000-days-production.up.railway.app/home',
+            developer: 'Sole Developer'
+        },
+        {
+            name: 'Technify-In-Python',
+            description: "A simple and intuitive social media website for tech enthusiasts to share valuable links with each other. Users can sign up to create, like, and comment on posts. Users have the freedom to delete and edit any of their posts while being logged in.",
+            technologies: 'Python, Flask, PyMySQL, SQLAlchemy, Bcrypt, Dotenv, Gunicorn, HTML, CSS, Javascript',
+            url: 'https://technify-by-cs.up.railway.app/',
+            developer: 'Sole Developer'
+        },
+        {
+            name: 'Taskmaster-Pro',
+            description: 'Take notes the easy way using Taskmaster Pro, which uses Bootstrap and JQuery technologies. Created tasks can be edited, deleted, and assigned due dates which is color coded to show if a due date is approaching or past due. Tasks can also be dragged around the application interface to various statuses, including "Completed", "In Progress" or deleted to the trash bin below.',
+            technologies: 'Javascript, JQuery, Moment.js,, Bootstrap, HTML, CSS',
+            url: 'https://cesarsiguencia.github.io/taskmaster-pro/',
+            developer: 'Sole Developer'
+        },
+        {
+            name: "Dine-N-Wine",
+            description: 'An application representing multiple hot plate recipes from all over the world! Users are able to save receipes that they like under "Favorites", filter receipes by category, or allow the application to pick one by random. In addition, users can look up for macronutrient information, ingredients, and amount per ingredient needed for the recipe that they choose.',
+            technologies: 'HTML, CSS, PureCSS, Javascript, APIs',
+            url: 'https://cesarsiguencia.github.io/dine-n-wine/',
+            developer: 'GROUP PROJECT: Front End Developer'
+        },
+        {
+            name: 'Weather-Dashboard',
+            description: 'Need to check full weather conditions of any city quickly? Use this application that will give you just that along with the temperature, wind speed, UV levels, and humidity. The user can also view the conditions up to five days from today. A convienient city search history is displayed for the user to compare conditions across multiple conditions.',
+            technologies: 'Javascript, HTML, CSS, Moment.js, Bootstrap, APIs',
+            url: 'https://cesarsiguencia.github.io/weather-dashboard/',
+            developer: 'Sole Developer'
+        }
+    ]
 
-        const generate = applications.map((app) => {
-            const lowercaseName = app.name.toLowerCase()
-            const formattedNames = app.name.split('-')
-            const displayedNames = formattedNames.join(' ')
+    const generate = applications.map((app) => {
+        const lowercaseName = app.name.toLowerCase()
+        const formattedNames = app.name.split('-')
+        const displayedNames = formattedNames.join(' ')
 
-            return(
+        return (
 
-                <div className="app-box" key={app.name}>
-                    <div className="container_wrapper">
-                        <div className="container--description">
-                            
+            <div className="app-box" key={app.name}>
+                <br />
+                <div className="container_wrapper">
+                    <div className="container--description">
 
-                            <div className="project-title">
-                                <h3 className="project-title--frame">{displayedNames}</h3>
-                            </div>
 
-                            <h5>{app.developer}</h5>
-                            <div>
-                                <div className="developer-technologies">
-                                    <p className="body-texts">{app.technologies}</p>
-                        
-                                </div>
-                                <div className="developer-github">
-                                    <a target="_blank" href={`https://github.com/cesarsiguencia/${lowercaseName}`}>
-                                            <img className="github-links"  src={github}>
-                                            </img>
-                                    </a>
-                                </div>
-                            </div>
-
-                       
-                            
+                        <div className="project-title">
+                            <h3 className="project-title--frame">{displayedNames}</h3>
                         </div>
-                    </div>
-                    <div className="container_wrapper">
-                        <div className="app_image">
-                            <a target="_blank" href={app.url} >
-                                <img className="container--picture" src= {require(`../../media/app-images/${app.name}.png`)} >
-                                </img>  
-                                <div className="container--overlay">
-                                    <p className="container---blurb"> {app.description}</p>  
-                                </div> 
-                            </a>
+
+                        <h5>{app.developer}</h5>
+                        <div>
+                            <div className="developer-technologies">
+                                <p className="body-texts">{app.technologies}</p>
+
+                            </div>
+                            <div className="developer-github">
+                                <a target="_blank" href={`https://github.com/cesarsiguencia/${lowercaseName}`}>
+                                    <img className="github-links" src={github}>
+                                    </img>
+                                </a>
+                            </div>
                         </div>
+
+
+
                     </div>
                 </div>
-            )
-        })
-
-        return(
-            <div>{generate}</div>
+                <div className="container_wrapper">
+                    <div className="app_image">
+                        <a target="_blank" href={app.url} >
+                            <img className="container--picture" src={require(`../../media/app-images/${app.name}.png`)} >
+                            </img>
+                            <div className="container--overlay">
+                                <p className="container---blurb"> {app.description}</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <br />
+            </div>
         )
+    })
 
-
-
-     
+    return (
+        <div>
+            <br />
+            {generate}
+        </div>
+    )
 }
 export default displayApp;
+
+
+
+// Old Apps commented out 
+
+{/* <div className="app-box">
+    <div className="container_wrapper">
+        <div className="container--description">
+            
+            <div className="project-title">
+                <h3 className="project-title--frame"> NFT-y</h3> 
+            </div>
+            
+            <h5>Back end developer</h5>
+            <div>
+                <div className="developer-technologies">
+                    <p className="body-texts">MySQL2, Sequelize, Node.js, Bcrypt, Express, RESTful API's, Env, Dotenv</p>
+                </div>
+
+                <div className="developer-github">
+                    <a target="_blank" href="https://github.com/K-G-J/nft-y">
+                        <img className="github-links" src={github}>
+                        </img>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className="container_wrapper">
+        <div className="app_image">
+            <a target="_blank" href='https://nft-y.herokuapp.com/'>
+                <img className="container--picture" src={require(`../../src/media/app-images/nft-y.png`)}>
+                </img>  
+                <div className="container--overlay">
+                    <p className="container---blurb">Browse through some of the most popular trending NFT groups at the moment, including Bored Ape Yacht Club, and learn about how these new artwork technologies work! Create an account to save NFTs to your favorites and chat with other members about NFT buying and selling opportunities. </p>  
+                </div> 
+            </a>
+        </div>
+    </div>
+</div> */}
+
+    // {   
+    //     name: 'Weather-Dashboard',
+    //     description: 'Need to check full weather conditions of any city quickly? Use this application that will give you just that along with the temperature, wind speed, UV levels, and humidity. The user can also view the conditions up to five days from today. A convienient city search history is displayed for the user to compare conditions across multiple conditions. Application uses local storage and APIs from openweathermap.org',
+    //     technologies: 'Javascript, HTML, CSS, Moment.js, Bootstrap, APIs',
+    // },
+    // {
+    //     name:'Show-Me-My-Schedule',
+    //     description:'Stay organized with all your tasks spread out throughout the day in traditional 9-5 blocks outlining your day. Blocks are color coded to show past, present, and future. Immediately edit a task or clear a task by saving it. ',
+    //     technologies: 'Bootstrap, JavaScript, JQuery, Moment.js, CSS'
+    // },
+    // {
+    //     name: 'Introductory-Javascript-Quiz',
+    //     description: 'Test your javascript skills with this quick quiz I built! Test yourself as many times as you would like as the app saves your scores at the end of each quiz. But hurry, you only have 60 seconds to answer as many javascript questions as possible. Happy studying!',
+    //     technologies: 'Javascript, HTML, CSS'
+    // },
+    // {
+    //     name: 'Save-My-Notes',
+    //     description: 'A classic note taking app that uses back end technologies to save your notes in their appropriate order.',
+    //     technologies: 'NoSQL, Sequelize, Express, RESTful route'
+    // },
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,138 +2,15 @@ import React from 'react'
 
 import Apps from '../Components/Apps/Apps'
 
-import github from '../media/icons/github-white.png'
-
 
 const Portfolio = () => {
 
     return(
         <div className="body-divs">
             <h3>Portfolio</h3>
-            <p className="body-texts">Hover over image to see a short description from one of my featured apps. 
-            Click on the image below to deploy application link. Click corresponding github logo to be redirected to its GitHub repository.</p>
-
-
-            {/* <div className="app-box">
-                    <div className="container_wrapper">
-                        <div className="container--description">
-                            
-
-                            <div className="project-title">
-                                <h3 className="project-title--frame">Dine-N-Wine</h3>
-                            </div>
-
-                            <h5>Front end developer</h5>
-                            <div>
-                                <div className="developer-technologies">
-                                    <p className="body-texts">HTML, CSS, Javascript, APIs</p>
-                                </div>
-
-                                <div className="developer-github">
-                                    <a target="_blank" href="https://github.com/cesarsiguencia/dine-n-wine">
-                                        <img className="github-links" src={github}>
-                                        </img>
-                                    </a>
-                                </div>
-                            </div>
-                           
-                        </div>
-                    </div>
-                    <div className="container_wrapper">
-                        <div className="app_image">
-                            <a target="_blank" href='https://cesarsiguencia.github.io/dine-n-wine/'>
-                                <img className="container--picture" src={require(`../../src/media/app-images/dine-n-wine.png`)}>
-                                </img>  
-                                <div className="container--overlay">
-                                    <p className="container---blurb"> An application representing multiple hot plate recipes from all over the world! Users are able to save receipes that they like under "Favorites", filter receipes by category, or allow the application to pick one by random. In addition, users can look up for macronutrient information, ingredients, and amount per ingredient needed for the recipe that they choose.</p>  
-                                </div> 
-                            </a>
-                        </div>
-                    </div>
-                </div> */}
-
-
-                {/* <div className="app-box">
-                        <div className="container_wrapper">
-                            <div className="container--description">
-                                
-                                <div className="project-title">
-                                    <h3 className="project-title--frame"> NFT-y</h3> 
-                                </div>
-                                
-                                <h5>Back end developer</h5>
-                                <div>
-                                    <div className="developer-technologies">
-                                        <p className="body-texts">MySQL2, Sequelize, Node.js, Bcrypt, Express, RESTful API's, Env, Dotenv</p>
-                                    </div>
-        
-                                    <div className="developer-github">
-                                        <a target="_blank" href="https://github.com/K-G-J/nft-y">
-                                            <img className="github-links" src={github}>
-                                            </img>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="container_wrapper">
-                            <div className="app_image">
-                                <a target="_blank" href='https://nft-y.herokuapp.com/'>
-                                    <img className="container--picture" src={require(`../../src/media/app-images/nft-y.png`)}>
-                                    </img>  
-                                    <div className="container--overlay">
-                                        <p className="container---blurb">Browse through some of the most popular trending NFT groups at the moment, including Bored Ape Yacht Club, and learn about how these new artwork technologies work! Create an account to save NFTs to your favorites and chat with other members about NFT buying and selling opportunities. </p>  
-                                    </div> 
-                                </a>
-                            </div>
-                        </div>
-                    </div> */}
-
-                
-       
+                <p className="body-texts">Hover cursor over image or hold and slide on image with your mobile to see a short description from one of my featured apps. 
+                Click on the image below to deploy application link. Click corresponding github logo to be redirected to its GitHub repository.</p>
             <Apps ></Apps>
-
-                {/* <div className="app-box">
-                        <div className="container_wrapper">
-                            <div className="container--description">
-                                
-                                <div className="project-title">
-                                    <h3 className="project-title--frame"> Save-My-Notes</h3> 
-                                </div>
-                                
-                                <h5>Back end developer</h5>
-                                <div>
-                                    <div className="developer-technologies">
-                                        <p className="body-texts">NoSQL, Sequelize, Express, RESTful routes</p>
-                                    </div>
-        
-                                    <div className="developer-github">
-                                        <a target="_blank" href="https://github.com/cesarsiguencia/save-my-notes">
-                                            <img className="github-links" src={github}>
-                                            </img>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="container_wrapper">
-                            <div className="app_image">
-                                <a target="_blank" href='https://frozen-refuge-80298.herokuapp.com/'>
-                                    <img className="container--picture" src={require(`../../src/media/app-images/save-my-notes.png`)}>
-                                    </img>  
-                                    <div className="container--overlay">
-                                        <p className="container---blurb"> A classic note taking app that uses back end technologies to save your notes in their appropriate order. </p>  
-                                    </div> 
-                                </a>
-                            </div>
-                        </div>
-                    </div> */}
-
-
-
-
-                    
-                
         </div>
     )
 }
