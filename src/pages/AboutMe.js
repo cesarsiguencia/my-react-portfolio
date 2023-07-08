50d1e4e9383d3c54c830d55c9148e2ab93719741
cesarsiguencia <cesar.siguencia@gmail.com>
2023-07-08 15:48:07 -0400

    2023/aboutm

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 import image from '../media/me/IMG_2178-copy.png'
-
-import landscape from '../media/me/home2.png'
 
 import bike from '../media/icons/cycling-bicycle.gif'
 
@@ -32,39 +30,34 @@ const About= () => {
                 </div>
          
                 <div className="aboutme-blocks">
-                    <p className="body-texts">I'm an aspiring web developer, music producer, and media content creator born and raised in NYC. Currently, I work at an East Harlem elementary school in administration and operations. I manage tuition, scholarships, internal school and student data, and work with a variety of students ranging from Pre-Kindergarten 3 to 8th grade.
-                    <br/>
-                    <br/>
-                    I am a full stack web developer constantly practicing and learning new web development technologies. I recently completed a coding bootcamp with Columbia University I enjoy creating productivity apps that help me stay organized and learning about design, which are the big reasons for why I decided to hop into the web development industry. I enjoy coding because the feeling of seeing your hard work and creativity come to full fruition is a reward in itself and being able to explain the technologies behind my work brings me great satisfaction.
-                  
-                  
+                    <p className="body-texts"> Greetings! My name is César (pronunced Say-sar) and I am a full stack web developer with a focus on developing productivity and social media applications. I completed a coding bootcamp with Columbia University and received my certification in 2022. You can find me practicing and learning about new web development technologies consistently, which is reflected on my most recent projects shown in my portfolio and on my GitHub. 
                     </p>
                     <p className="body-texts bold-and-left">
                     <br/>
-                 
                     Some of the current projects I am working on include the following:
                     <br/>
-
-        
-                 
                     <br/>
 
-                    1 - A blog and sign up application for a special event I will be hosting in the fall of 2023 for when I turn exactly 10,000 days old!
+                    1 - A sign up application for a special event I will be hosting in the fall of 2023 for when I turn exactly 10,000 days old!
                    
                     <br/>
                     <br/>
-                    2 - A social media website outlining my journey coding post bootcamp graduation.
+                    2 - A gallery website on my favorite spots in my neighborhood Washington Heights, which is iconic in the musical 'In The Heights'.
                     <br/>
                     <br/>
-                    3 - A website showcasing the various CSS buttons, swtiches, sliders, and other event triggering actions that I have practiced and learned that display an enjoyable and eye captivating user experience on my various projects.
-
-                    <br/>
-                    <br/>
+                    3 - A MERN application promoting a new side business as an Apple product fixer, where I will showcase various projects on broken devices I have fixed, including iPods, iPhones, and Apple monitors. I will include a form for anyone to contact me and give me details about the service they wish to receive on their device.
                     </p>
-                    
-
                     <p className="body-texts">
-                    Outside of my job and hobbies, you can find me weight training at the gym, running, cycling, cooking with loved ones, trying various ethnic cuisines around the city, or relaxing at uptown parks birdwatching and sightseeing. I identify as Latino and speak Spanish and English fluently. I can read and speak Chinese at an intermediate level from courses I took in high school, college, and a summer study abroad. </p>
+                    <br/>
+                    <p className="body-texts bold-and-left">So why did I choose coding?</p>
+                     When coding, there are undoubtly many challenges, especially when one implements technologies still very foreign to them. But I never get tired of the feel-good/satisfying instant moment I get when something works the way I intended after debugging for some time! It's a simple, yet proud feeling that any developer I'm sure will understand.
+                    <br/>
+                    <br/>
+                    I have always been curious to see how and why something works. Because of this inquisitive nature, I am able to be persistent, so I know that my success in any developer challenge will be inevitable. This is the fulfillment building applications gives me and why challenges fuel these passions even more.
+                    <br/>
+                    <br/>
+                    <p className="body-texts bold-and-left">Work and Hobbies</p>
+                    For almost five years, I've worked at an East Harlem elementary school in operations. On my own time, you can find me weight training, running, cycling, cooking with loved ones, or relaxing at uptown parks birdwatching and sightseeing. I identify as Latino and speak Spanish and English fluently. I can read and speak Chinese at an intermediate level from courses I took in high school, college, and a summer study abroad. </p>
 
 
                     <div className="aboutme--gifs">
@@ -119,12 +112,12 @@ const About= () => {
             </div>
 
        
-            <div className="aboutme">
+            {/* <div className="aboutme">
                 <div className="aboutme-blocks">
                     <img className="profile" src={landscape} width="100%"/>
                 </div>
            
-            </div>
+            </div> */}
 
 
          
