699726922d9db9dcc53deff9ef1cffd1e9cd8d0c
cesarsiguencia <cesar.siguencia@gmail.com>
2023-07-12 22:57:55 -0400

    successful converted skills-list div to it's own component, adhearing to DRY principles

---
+++ src/Components/Icons/icons.js
@@ -0,0 +1,66 @@
+import React, { useRef, useEffect } from 'react'
+
+import html from '../../media/icons/skills-icons/html5.png'
+import css from '../../media/icons/skills-icons/css3.png'
+import js from '../../media/icons/skills-icons/js.png'
+import jquery from '../../media/icons/skills-icons/jquery.png'
+import graphql from '../../media/icons/skills-icons/graphql.png'
+import node from '../../media/icons/skills-icons/nodejs.png'
+import express from '../../media/icons/skills-icons/express.png'
+import mysql from '../../media/icons/skills-icons/mysql.png'
+import react from '../../media/icons/skills-icons/react.png'
+import sequelize from '../../media/icons/skills-icons/sequelize.png'
+import mongoose from '../../media/icons/skills-icons/mongoose.png'
+import bootstrap from '../../media/icons/skills-icons/bootstrap.png'
+import api from '../../media/icons/skills-icons/api.png'
+import mongodb from '../../media/icons/skills-icons/mongodb.png'
+import handlebars from '../../media/icons/skills-icons/handlebars.png'
+import nosql from '../../media/icons/skills-icons/nosql.png'
+import python from '../../media/icons/skills-icons/python.png'
+import git from '../../media/icons/skills-icons/git.png'
+import github from '../../media/icons/skills-icons/github-logo.png'
+import vscode from '../../media/icons/skills-icons/vscode.png'
+
+const iconEffect = () => {
+
+    const techArrays = [
+        {
+            name: 'Front End',
+            links: [
+                { url: html }, { url: css }, { url: js }, { url: jquery }, { url: bootstrap }, { url: react }, { url: handlebars }
+            ]
+        },
+        {
+            name: 'Back End',
+            links: [
+                { url: node }, { url: python }, { url: api }, { url: express }, { url: mysql }, { url: sequelize }, { url: mongodb }, { url: graphql }, { url: mongoose }, { url: nosql }
+            ]
+        },
+        {
+            name: 'Developer Technologies',
+            links: [
+                { url: git }, { url: github }, { url: vscode }
+            ]
+        }
+    ]
+
+    const myRef = useRef()
+
+    return (
+
+        techArrays.map((singleArray) => {
+            return <div key={singleArray.name} ref={myRef}>
+                <h5>{singleArray.name}</h5>
+                <div className='technologies'>
+                    {singleArray.links.map((tech) => {
+                        return <img className='technologies-logos' src={tech.url} key={tech.url} alt={tech.url}>
+                        </img>
+                    })}
+                </div>
+            </div>
+        })
+    )
+}
+
+export default iconEffect
+
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,29 +1,13 @@
 import React, {useRef, useEffect} from 'react'
-import general_resume from './Cesar_Siguencia-Resume-General.pdf'
+// import general_resume from './Cesar_Siguencia-Resume-General.pdf'
 import developer_resume from './Cesar_Siguencia-Resume-Developer.pdf'
+import Icons from '../Components/Icons/icons'
 
+
+import other from '../media/icons/document.png'
 import image from './resume.png'
 
-import html from '../media/icons/skills-icons/html5.png'
-import css from '../media/icons/skills-icons/css3.png'
-import js from '../media/icons/skills-icons/js.png'
-import jquery from '../media/icons/skills-icons/jquery.png'
-import graphql from '../media/icons/skills-icons/graphql.png'
-import node from '../media/icons/skills-icons/nodejs.png'
-import express from '../media/icons/skills-icons/express.png'
-import mysql from '../media/icons/skills-icons/mysql.png'
-import react from '../media/icons/skills-icons/react.png'
-import sequelize from '../media/icons/skills-icons/sequelize.png'
-import mongoose from '../media/icons/skills-icons/mongoose.png'
-import bootstrap from '../media/icons/skills-icons/bootstrap.png'
-import api from '../media/icons/skills-icons/api.png'
-import mongodb from '../media/icons/skills-icons/mongodb.png'
-import handlebars from '../media/icons/skills-icons/handlebars.png'
-import nosql from '../media/icons/skills-icons/nosql.png'
-import python from '../media/icons/skills-icons/python.png'
-import git from '../media/icons/skills-icons/git.png'
-import github from '../media/icons/skills-icons/github-logo.png'
-import vscode from '../media/icons/skills-icons/vscode.png'
+
 
 const Resume = () => {
     const myRef = useRef()
@@ -65,7 +49,8 @@ const Resume = () => {
                         <a href={general_resume} download><h5>General Resume</h5></a>
                         <br/> */}
                     
-                        <a href={developer_resume} download><h5>Web Developer / Software Engineer Resume</h5></a>
+                        <a href={developer_resume} download><img className='logos-icons logos-contactpg'style={{borderRadius: '200px'}}src={other}>
+                        </img></a>
 
                         <br/>
                         <p className="body-texts bold-and-left">Languages</p>
@@ -77,52 +62,11 @@ const Resume = () => {
           
                 </div>
                 
-                <div className="skills-blocks">
+                <div className="skills-blocks" ref={myRef}>
                     <h3>Web Development Skills</h3>
 
                     <div className="skills--lists" >
-                        <div>
-                            <h5>Front End</h5>
-                 
-                            <div className="technologies" ref={myRef}>
-                                <img className="technologies-logos" src={html}/>
-                                <img className="technologies-logos" src={css}/>
-                                <img className="technologies-logos" src={js}/>
-                                <img className="technologies-logos" src={jquery}/>
-                                <img className="technologies-logos" src={bootstrap}/>
-                                <img className="technologies-logos" src={react}/>
-                                <img className="technologies-logos" src={handlebars}/>
-
-                               
-                            </div>
-                        </div>
-                        <div>
-                            <h5>Back End</h5>
-                            <div className="technologies">
-                                
-                                <img className="technologies-logos" src={node}/>
-                                <img className="technologies-logos" src={python}/>
-                                <img className="technologies-logos" src={api}/>
-                                <img className="technologies-logos" src={express}/>
-                                <img className="technologies-logos" src={mysql}/>
-                                <img className="technologies-logos" src={sequelize}/>
-                                <img className="technologies-logos" src={mongodb}/>
-                                <img className="technologies-logos" src={graphql}/>
-                                <img className="technologies-logos" src={mongoose}/>
-                                <img className="technologies-logos" src={nosql}/>
-                                
-
-                            </div>
-                        </div>
-
-                        <div>
-                            <h5>Developer Technologies</h5>
-                            <div className="technologies">
-                                <img className="technologies-logos" src={git}/>
-                                <img className="technologies-logos" src={github}/>
-                                <img className="technologies-logos" src={vscode}/>
-                            </div>
-                        </div>
+                        <Icons></Icons>
                     </div>
 
                 </div>
