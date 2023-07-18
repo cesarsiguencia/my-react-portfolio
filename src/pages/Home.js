321380ca8c97f0640840f2a0cfb9f3625f96c544
cesarsiguencia <cesar.siguencia@gmail.com>
2023-07-17 23:21:08 -0400

    added portfolio button in home pg"

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,67 +34,85 @@ const Home = () => {
     }
 
     var growFontEffect = useEffect(() => {
-        const selectedFonts = document.querySelector('.home-transitions')
+        const selectedFonts = document.querySelectorAll('.home-transitions')
 
-        if (screenWidth > 1000) {
-            setInterval(() => {
-                selectedFonts.style = "font-size: 25px"
-            }, 1500)
+        selectedFonts.forEach((font) => {
+            if (screenWidth > 1000) {
+                setInterval(() => {
+                    // font.style = "font-size: 25px"
+                    font.style.scale = "0.7"
+                }, 1500)
 
-            setInterval(() => {
-                selectedFonts.style = "font-size: 20px"
-            }, 3000)
-        }
+                setInterval(() => {
+                    // font.style = "font-size: 20px"
+                    font.style.scale = "1"
+                }, 3000)
+            }
+
+        })
+
     }, [])
 
     return (
 
+        <div onLoad={growFontEffect}>
+            <div className="home-div">
+                <div className="home-bcgs-color" onLoad={homeParallax}>
+                    <img className="bcg-2" id="bcg-2" src={portrait2} />
 
-        <div className="home-div">
-            <div className="home-bcgs-color" onLoad={homeParallax}>
-                <img className="bcg-2" id="bcg-2" src={portrait2} />
+                    <div className="hidden-intro">
 
-                <div className="hidden-intro">
+                        <h3 >"Welcome to my page!</h3>
 
-                    <h3 >"Welcome to my page!</h3>
+                        <h4>Hello, Hola, 你好!</h4>
 
+
+                        <div className="hidden-blurb">
+                            <h6 className="body-texts home-pg-font">Thank you for visiting my page! Check out the projects I have been working on!</h6>
+                            {/* <div style={{border: '3px solid orange'}} href='./portfolio'>
+                            <p className="body-texts">Go To My Porfolio</p>
+                        </div> */}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="home-bcgs-bg">
+                    <img className="bcg-1" id="bcg-1" src={portrait1} width={backgroundWidth} />
+
+                    <div className="hidden-intro" >
+                        <div>
+                            <h6 className="body-texts home-pg-font home-transitions" >* Slide Mouse Horizontally! *</h6>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+                <div className="pix-1000">
+                    <h3>"Welcome to my page!</h3>
                     <h4>Hello, Hola, 你好!</h4>
-
-
-                    <div className="hidden-blurb">
-                        <h6 className="body-texts home-pg-font">Thank you for visiting my page! Check out the projects I have been working on!</h6>
-                        {/* <div style={{border: '3px solid orange'}} href='./portfolio'>
-                            <p className="body-texts">Go To My Porfolio</p>
-                        </div> */}
-                    </div>
-                </div>
-            </div>
-
-            <div className="home-bcgs-bg" onLoad={growFontEffect}>
-                <img className="bcg-1" id="bcg-1" src={portrait1} width={backgroundWidth} />
-
-                <div className="hidden-intro" >
                     <div>
-                        <h6 className="body-texts home-pg-font home-transitions" >* Slide Mouse Horizontally! *</h6>
+                        <h6 className="body-texts"> Thank you for visiting my page! Check out the projects I have been working on! </h6>
                     </div>
                 </div>
-            </div>
 
-            <div className="pix-1000">
-                <h3>"Welcome to my page!</h3>
-                <h4>Hello, Hola, 你好!</h4>
-                <div>
-                    <h6 className="body-texts"> Thank you for visiting my page! Check out the projects I have been working on! </h6>
-                    {/* <div style={{border: '3px solid orange', borderRadius: '50px' , margin: '20px auto'}} href='/portfolio'>
-                            <p className="body-texts">Go To My Porfolio</p>
-                        </div> */}
+
+            </div >
+
+            <a href='https://cesarsiguencia.github.io/my-react-portfolio/portfolio' >
+                <div className="button-holder">
+                    <p className="body-texts home-pg-font home-transitions portfolio-button">Go To My Porfolio!</p>
                 </div>
-            </div>
+            </a>
         </div>
+
 
     )
 
 }
+
+
 
 export default Home
 
