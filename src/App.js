a098d2e7a9e781ba18b7eae2bfdaca9208b3315e
cesarsiguencia <cesar.siguencia@gmail.com>
2023-07-18 15:54:45 -0400

    added delay to App on load after loader disappears

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import './loader.css'
 // import $ from 'jquery'
-
-
-
-
 import LoadingPg from './Components/Loading/Loading'
-
-
-
-
 import Header from './Components/Header/Header';
-
 import Footer from './Components/Footer/Footer'
-
-
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const Home = React.lazy(()=> import('./pages/Home'))
-const AboutMe  = React.lazy(() => import('./pages/AboutMe'))
-const Portfolio = React.lazy(()=> import('./pages/Portfolio'))
-const Contact = React.lazy(()=> import('./pages/Contact'))
-const Resume = React.lazy(()=> import('./pages/Resume'))
+const Home = React.lazy(() => import('./pages/Home'))
+const AboutMe = React.lazy(() => import('./pages/AboutMe'))
+const Portfolio = React.lazy(() => import('./pages/Portfolio'))
+const Contact = React.lazy(() => import('./pages/Contact'))
+const Resume = React.lazy(() => import('./pages/Resume'))
 // const Footer = React.lazy(()=> import('./Components/Footer/Footer'))
 
 function App() {
-
-  // $(window).on("load", function(){
-  //   $(".loader-wrapper").fadeOut("slow")
-  // })
-
-  // $(window).on("mobileinit", function(){
-  //   $(".loader-wrapper").fadeOut("slow")
-  // })
-
-
-
-  // window.addEventListener("load", () => {
-  //   const LoadAni = document.querySelector('.loader-wrapper')
-  //   LoadAni.style.opacity = 0
-  // } )
 
   const [loading, setLoading] = useState(false)
 
@@ -49,9 +23,18 @@ function App() {
     setLoading(true)
     setTimeout(() => {
       setLoading(false)
-    }, 2000)
-  }, [])
 
+    }, 1000)
+
+    setTimeout(()=>{
+      const application = document.querySelector('.App')
+      if(application){
+        application.style.opacity = 1
+
+      }
+    }, 1200)
+  }, [])
+  
   const pages = [
     {
       name: "About Me"
@@ -63,69 +46,60 @@ function App() {
       name: "Contact"
     },
     {
-      name:"Resume & Skills"
+      name: "Resume & Skills"
     }
   ]
 
   const [currentPage, setCurrentPage] = useState(pages)
 
-  var grayScaleLoad = function(){
-    // const image = document.querySelector('.profile')
+  var grayScaleLoad = function () {
     const theBody = document.querySelector('.body-divs')
     const homeBody = document.querySelector('.home')
 
-    if(theBody){
-      theBody.addEventListener("touchmove", ()=>{
+    if (theBody) {
+      theBody.addEventListener("touchmove", () => {
         theBody.style = 'filter: grayscale(0%)';
         theBody.style.opacity = 1
 
       })
-      theBody.addEventListener("mouseover", ()=>{
+      theBody.addEventListener("mouseover", () => {
         theBody.style = 'filter: grayscale(0%)';
         theBody.style.opacity = 1
       })
-      theBody.addEventListener("mouseout", ()=>{
+      theBody.addEventListener("mouseout", () => {
         theBody.style = 'filter: grayscale(100%)'
         theBody.style.opacity = 1
       })
     }
 
-    if(homeBody){
+    if (homeBody) {
       homeBody.style.opacity = 1
     }
   }
 
-  var pageEffects = function(){
-    window.scrollTo(0,0)
+  var pageEffects = function () {
+    window.scrollTo(0, 0)
 
 
     const bodyDiv = document.querySelector('.body-divs')
-    if(bodyDiv){
-      bodyDiv.style.opacity= 1
+    if (bodyDiv) {
+      bodyDiv.style.opacity = 1
     }
   }
 
   const [portfolioClicked, setPortfolioClicked] = useState(false)
 
-  
+
 
   return (
     <Router >
       {
-        // loading && !portfolioClicked ?
-        //   (
-        //     <div className="loader-wrapper">
-        //       <div className="loader-box">
-        //         <div className="loader">
-        //         </div>
-        //         <br />
-        //         <p className='h1 links'>I am loading!</p>
-        //       </div>
-        //     </div>
+        loading ?
+          (
+            <LoadingPg></LoadingPg>
+          ) : (
 
-        //   ) : (
-            
-            <div className="App" onLoad={pageEffects} portfolioClicked={portfolioClicked} setPortfolioClicked={setPortfolioClicked}>
+            <div className="App" onLoad={pageEffects}>
               <Header
                 pages={pages}
                 setCurrentPage={setCurrentPage}
@@ -135,31 +109,31 @@ function App() {
                 <Routes>
                   <Route portfolioClicked={portfolioClicked}
                     path="/my-react-portfolio"
-                    element={<React.Suspense fallback= {<LoadingPg></LoadingPg>} 
-                  
-                    ><Home/></React.Suspense>}
+                    element={<React.Suspense fallback={<LoadingPg></LoadingPg>}
+
+                    ><Home /></React.Suspense>}
                   />
                   <Route
                     path="/my-react-portfolio/aboutme"
-                    element={<React.Suspense fallback= {<LoadingPg></LoadingPg>}><AboutMe/></React.Suspense>}
+                    element={<React.Suspense fallback={<LoadingPg></LoadingPg>}><AboutMe /></React.Suspense>}
                   />
                   <Route
                     path="/my-react-portfolio/portfolio"
-                    element={<React.Suspense fallback= {<LoadingPg></LoadingPg>}><Portfolio/></React.Suspense>}
+                    element={<React.Suspense fallback={<LoadingPg></LoadingPg>}><Portfolio /></React.Suspense>}
                   />
                   <Route
                     path="/my-react-portfolio/contact"
-                    element={<React.Suspense fallback= {<LoadingPg></LoadingPg>}><Contact/></React.Suspense>}
+                    element={<React.Suspense fallback={<LoadingPg></LoadingPg>}><Contact /></React.Suspense>}
                   />
                   <Route
                     path="/my-react-portfolio/resume"
-                    element={<React.Suspense fallback= {<LoadingPg></LoadingPg>}><Resume/></React.Suspense>}
+                    element={<React.Suspense fallback={<LoadingPg></LoadingPg>}><Resume /></React.Suspense>}
                   />
                 </Routes>
               </div>
               <Footer></Footer>
             </div>
-          // )
+          )
       }
     </Router>
   );
--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
 function loadingPg() {
-    return(
+
+    return (
         <div className="loader-wrapper">
-               <div className="loader-box">
-                 <div className="loader">
-                 </div>
-                 <br />
-                 <p className='h1 links'>I am loading!</p>
-             </div>
-         </div>
+            <div className="loader-box">
+                <div className="loader">
+                </div>
+                <br />
+                <p className='h1 links'>I am loading!</p>
+            </div>
+        </div>
     )
 }
 
