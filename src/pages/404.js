f2a33f86ce772a77c0b665928fdb869b993b59c9
cesarsiguencia <cesar.siguencia@gmail.com>
2023-09-16 08:50:14 -0400

    added 404 pg

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const div404 = () => {
+    return(
+        <div className="header-hero">
+            <div className="header-text">
+                <h1><strong className="red-font">404</strong> | Page not found</h1>
+                <p className="header-slogan">The link may be broken or does not exist.</p>
+            </div>
+        </div>
+    )
+}
+
+export default div404
