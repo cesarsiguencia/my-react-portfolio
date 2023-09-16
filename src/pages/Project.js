d38ea19a4ca6ec75feffafc3b1a5c96dca398db3
cesarsiguencia <cesar.siguencia@gmail.com>
2023-09-15 21:44:20 -0400

    project page customized with media queries

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import { opacityEffect } from "../utils/helpers";
 import GitHubLogo from '../media/icons/github.svg'
+import LaunchLogo from '../media/icons/launch.avif'
 
 const projectPg = ({ singleApp }) => {
 
@@ -26,58 +27,79 @@ const projectPg = ({ singleApp }) => {
                 {
                     returnedName &&
                     <>
-                    <div  >
-                        <div className='title-block left'>
-                            <div className='title'>
-                                <h2>{returnedName}</h2>
+                        <div  >
+                            <div className='title-block left'>
+                                <div className='title'>
+                                    <h2>{returnedName}</h2>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="row">
-                            <div className="col">
-                                <h4 className="left">Role: {singleApp.developer}</h4>
+                            <div className="row singleApp-block">
+                                <div className="col align-ver">
+                                    <div>
+                                        <h4 className="left">Role: {singleApp.developer}</h4>
 
-                                <p className="body-texts left">{singleApp.description}</p>
+                                        <p className="body-texts left">{singleApp.description}</p>
 
-                                <div style={{display: 'flex', flexWrap: 'wrap'}}>
-                                    {singleApp.technologies.map((tech, i)=>{
-                                        return(
-                                            <div key={i} style={{margin:'5px 5px'}}>
-                                                 <p className='technologies-names'>{tech}</p>
-                                            </div>
-                                           
-                                        )
-                                    })}
-                                   
-                                </div>
+                                        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                                            {singleApp.technologies.map((tech, i) => {
+                                                return (
+                                                    <div key={i} style={{ margin: '5px 5px' }}>
+                                                        <p className='technologies-names'>{tech}</p>
+                                                    </div>
+                                                )
+                                            })}
 
-                                <div className="left">
-                                    <a target='_blank' href={`https://github.com/cesarsiguencia/${singleApp.name}`}>
-                                    <img alt='github logo' src={GitHubLogo} style={{width: '75px', height: '75px', borderRadius: '100px'}}></img>
-
-                                    </a>
-                                   
-                                </div>
-
-                                <div>
-                                    {singleApp.heroku_cred && 
-                                        <div>
-                                            <h5 className="left">This app requires credentials!</h5>
-                                            <p className="body-texts left">To experience all the features of the app, create a new account or use the sample credentials of the test account below:</p>
-                                            <br/>
-                                            <p className="body-texts left">Username: <span className="bold red-font">{singleApp.heroku_cred.username}</span></p>
-                                            <p className="body-texts left"> Password or Zipcode: <span className="bold red-font">{singleApp.heroku_cred.password}</span></p>
                                         </div>
-                                    }
+
+                                        <div>
+                                            <a target='_blank' style={{display:'inline-block'}} href={`https://github.com/cesarsiguencia/${singleApp.name}`}>
+                                                <div className="home-btns center" style={{ color: 'black', display: 'flex', width: '300px', margin: '15px auto' }}>
+
+                                                    <p className="h5">Visit GitHub Repository</p>
+                                                    <div className="align-ver">
+                                                        <img alt='github logo' src={GitHubLogo} style={{ display: 'block', height: '35px', margin: '0 10px' }}></img>
+                                                    </div>
+                                                </div>
+                                            </a>
+
+                                            <a target='_blank'  style={{display:'inline-block'}} href={singleApp.url}>
+                                                <div className="home-btns center" style={{ color: 'black', display: 'flex', width: '300px', margin: '15px auto' }}>
+
+                                                    <p className="h5">Deploy App</p>
+                                                    <div className="align-ver">
+                                                        <img alt='a launch logo' src={LaunchLogo} style={{ display: 'block', height: '35px', margin: '0 10px' }}></img>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+
+                                        <div>
+                                            {singleApp.heroku_cred &&
+                                                <div>
+                                                    <p className="h5 left">This app requires credentials!</p>
+                                                    <p className="body-texts left">To experience all the features of the app, <span className="red-font bold">create a new account</span> or <span className="red-font bold">use the sample credentials </span>of the test account below:</p>
+
+                                                    <div style={{ margin: '15px auto' }}>
+                                                        <p className="body-texts left bold">Username: <span className="green-font">{singleApp.heroku_cred.username}</span></p>
+                                                        <p className="body-texts left bold"> Password or Zipcode: <span className="green-font">{singleApp.heroku_cred.password}</span></p>
+                                                    </div>
+
+
+                                                </div>
+                                            }
+                                        </div>
+
+                                    </div>
+
                                 </div>
-                            </div>
 
-                            <div className="col">
-                                <img style={{ height: '600px', width: '450px' }} alt={`Full view of the ${returnedName} app`} src={require(`../media/app-images/${singleApp.name}.avif`)}></img>
-                            </div>
+                                <div className="col align-ver center">
+                                    <img className='single-app-image' alt={`Full view of the ${returnedName} app`} src={require(`../media/app-images/${singleApp.name}.avif`)}></img>
+                                </div>
 
+                            </div>
                         </div>
-                    </div>
                     </>
                 }
             </div>
